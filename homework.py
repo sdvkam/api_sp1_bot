e290dd2c8afe2bfdbf6230abe46d7a3ce3b10d12
Dmitry <sdvkam@gmail.com>
2021-10-01 11:27:53 +1200

    First reviev_all doing

--- homework.py
+++ homework.py
@@ -1,26 +1,31 @@
 import logging
 import os
+import sys
 import time
 from logging import FileHandler
 
 import requests
 import telegram
 from dotenv import load_dotenv
-from requests.exceptions import ConnectionError
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger.setLevel(logging.DEBUG)
-handler = FileHandler('my_logger.log', mode='w')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+handler_file = FileHandler('my_logger.log', mode='w')
+handler_file.setFormatter(formatter)
+logger.addHandler(handler_file)
+handler_console = logging.StreamHandler(sys.stdout)
+handler_console.setFormatter(formatter)
+logger.addHandler(handler_console)
 
 bot = telegram.Bot(TELEGRAM_TOKEN)
 
@@ -40,29 +45,58 @@ def get_homeworks(current_timestamp):
     url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=payload)
+        return homework_statuses.json()
+    except requests.exceptions.RequestException as e:
+        return f'Проблема с доступом к API с домашкой: {e}'
+    except Exception as e:
+        return f'При доступе к API с домашкой произошли непонятки: {e}'
+
+
+def check_format_api_answer(api_anwser):
+    list_right_api_status = ['rejected', 'reviewing', 'approved']
+    try:
+        if len(api_anwser['homeworks']) == 0:
+            return True
+        if not isinstance(api_anwser['homeworks'][0]['homework_name'], str):
+            return False
+        if not isinstance(api_anwser['homeworks'][0]['status'], str):
+            return False
+        if api_anwser['homeworks'][0]['status'] not in list_right_api_status:
+            return False
+        return True
+    except Exception:
+        return False
 
 
 def send_message(message):
-    return bot.send_message(chat_id=CHAT_ID, text=message)
+    try:
+        return bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception as e:
+        return f'Сообщение не отправлено из-за ошибки: {e}'
 
 
 def main():
-    current_timestamp = int(time.time())
+    current_timestamp = 0
     while True:
         logger.debug('Бот проснулся!')
         try:
             list_homework_status = get_homeworks(current_timestamp)
-            for one_homework in list_homework_status['homeworks']:
-                message = parse_homework_status(one_homework)
-                send_message(message)
-                logger.info(f'Сообщение отправлено: {message}')
+            if isinstance(list_homework_status, str):
+                logger.error(list_homework_status)
+            elif not check_format_api_answer(list_homework_status):
+                logger.error('Неверный ответ сервера')
+            elif len(list_homework_status['homeworks']) > 0:
+                last_howmework = list_homework_status['homeworks'][0]
+                message = parse_homework_status(last_howmework)
+                result = send_message(message)
+                if isinstance(result, str):
+                    logger.error(result)
+                else:
+                    logger.info(f'Сообщение отправлено: {message}')
                 current_timestamp = int(time.time())
-            time.sleep(5 * 60)
-        except ConnectionError as e:
-            logger.error(f'Нет связи: {e}')
-            time.sleep(5)
+            time.sleep(5 * 2)
         except Exception as e:
             message = f'Бот упал с ошибкой: {e}'
             logger.error(message)
